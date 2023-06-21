b6515f4df2c9baa6abb39b4c0ac7b149c615fac0
Douglas Barone <douglasvbarone@gmail.com>
2023-06-21 15:02:54 -0400

    Init migrations

--- prisma/migrations/20230620191614_cascade/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "PrinterStatus" DROP CONSTRAINT "PrinterStatus_printerId_fkey";
-
--- AddForeignKey
-ALTER TABLE "PrinterStatus" ADD CONSTRAINT "PrinterStatus_printerId_fkey" FOREIGN KEY ("printerId") REFERENCES "Printer"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20230621190239_init/migration.sql
+++ prisma/migrations/20230621190239_init/migration.sql
@@ -10,6 +10,7 @@ CREATE TABLE "User" (
     "thumbnailPhoto" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "campus" TEXT,
     "roles" "Role"[] DEFAULT ARRAY['USER']::"Role"[],
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
@@ -21,7 +22,6 @@ CREATE TABLE "Printer" (
     "friendlyName" TEXT,
     "location" TEXT,
     "serialNumber" TEXT,
-    "hostname" TEXT,
     "ip" TEXT NOT NULL,
     "model" TEXT NOT NULL,
     "blackTonerModel" TEXT,
@@ -30,6 +30,7 @@ CREATE TABLE "Printer" (
     "yellowTonerModel" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "networkId" INTEGER NOT NULL,
 
     CONSTRAINT "Printer_pkey" PRIMARY KEY ("id")
 );
@@ -42,10 +43,20 @@ CREATE TABLE "PrinterStatus" (
     "tonerMagentaLevel" INTEGER,
     "tonerYellowLevel" INTEGER,
     "counter" INTEGER NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "printerId" INTEGER NOT NULL,
 
     CONSTRAINT "PrinterStatus_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Network" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "shortName" TEXT NOT NULL,
+    "cidr" TEXT NOT NULL,
+
+    CONSTRAINT "Network_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -57,5 +68,23 @@ CREATE UNIQUE INDEX "Printer_serialNumber_key" ON "Printer"("serialNumber");
 -- CreateIndex
 CREATE UNIQUE INDEX "Printer_ip_key" ON "Printer"("ip");
 
+-- CreateIndex
+CREATE INDEX "PrinterStatus_timestamp_idx" ON "PrinterStatus"("timestamp");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Network_name_key" ON "Network"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Network_shortName_key" ON "Network"("shortName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Network_cidr_key" ON "Network"("cidr");
+
+-- CreateIndex
+CREATE INDEX "Network_id_idx" ON "Network"("id");
+
 -- AddForeignKey
-ALTER TABLE "PrinterStatus" ADD CONSTRAINT "PrinterStatus_printerId_fkey" FOREIGN KEY ("printerId") REFERENCES "Printer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Printer" ADD CONSTRAINT "Printer_networkId_fkey" FOREIGN KEY ("networkId") REFERENCES "Network"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PrinterStatus" ADD CONSTRAINT "PrinterStatus_printerId_fkey" FOREIGN KEY ("printerId") REFERENCES "Printer"("id") ON DELETE CASCADE ON UPDATE CASCADE;
