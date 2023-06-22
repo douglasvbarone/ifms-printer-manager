6e779c641556d96c2e0fda1eea3b6c367ef76fa4
Douglas Barone <douglasvbarone@gmail.com>
2023-06-22 08:04:10 -0400

    Added Logger Middleware

---
+++ src/middlewares/loggerMiddleware.ts
@@ -0,0 +1,17 @@
+import { Response, NextFunction, Request } from 'express'
+
+export async function loggerMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.log(
+    'Request:',
+    new Date().toLocaleString(),
+    req.method,
+    req.url,
+    res.locals.user.username,
+    res.locals.user.displayName
+  )
+  next()
+}
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 
 import { populateUserMiddleware } from './middlewares/populateUserMiddleware.js'
 import { authMiddleware } from './middlewares/authMiddleware.js'
+import { loggerMiddleware } from './middlewares/loggerMiddleware.js'
 
 import LoginRouter from './controllers/LoginController.js'
 import PrinterRouter from './controllers/PrinterController.js'
@@ -15,6 +16,7 @@ app.use('/', express.static('public'))
 
 app.use(bodyParser.json())
 app.use(populateUserMiddleware)
+app.use(loggerMiddleware)
 
 app.use('/api/login', LoginRouter)
 app.use('/api/printer', PrinterRouter)
