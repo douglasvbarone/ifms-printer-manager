bead7172e0a0d824c850efc3f1c50fd0ac7f6baf
Douglas Barone <douglasvbarone@gmail.com>
2023-06-29 12:22:51 -0400

    Alpha badge

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,4 +1,8 @@
 <template>
+  <div class="alpha-banner py-1 px-3 bg-amber text-black">
+    <v-icon size="small" icon="mdi-bug" />
+    Em desenvolvimento
+  </div>
   <v-app>
     <v-main><RouterView /></v-main>
   </v-app>
@@ -11,4 +15,15 @@ const appStore = useAppStore()
 
 appStore.fetchMe()
 </script>
-./store/appStore
+<style>
+.alpha-banner {
+  position: fixed;
+  bottom: 5px;
+  right: 5px;
+  z-index: 99999;
+  opacity: 0.7;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.12), 0 2px 2px rgba(0, 0, 0, 0.12),
+    0 4px 4px rgba(0, 0, 0, 0.12), 0 8px 8px rgba(0, 0, 0, 0.12),
+    0 16px 16px rgba(0, 0, 0, 0.12);
+}
+</style>
