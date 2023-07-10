a0c5613d815c00206937a5316311b8bb86a9e2ff
Douglas Barone <douglasvbarone@gmail.com>
2023-07-10 08:24:02 -0400

    404 v2

--- web/src/views/404.vue
+++ web/src/views/404.vue
@@ -1,10 +1,25 @@
 <template>
-  <v-container class="page" fluid>
-    <h1 class="text-h1">404</h1>
-    <p>Página não encontrada...</p>
-    <br />
+  <v-container class="page text-center fill-height">
+    <div style="width: 100%">
+      <h1 class="text-h1 mb-16">404</h1>
+      <p class="mb-16 text-h5">
+        <v-icon start>mdi-file-search-outline</v-icon>
+        Página não encontrada...
+      </p>
+      <br />
 
-    <p>Talvez tenha acabado o toner...</p>
-    <p>Chacoalhe o computador, as vezes resolve... 🤷</p>
+      <p>Talvez tenha acabado o toner...</p>
+      <p>Chacoalhe o computador, as vezes resolve... 🤷</p>
+
+      <v-btn
+        size="x-large"
+        class="mt-16"
+        color="primary"
+        :to="{ name: 'Home' }"
+      >
+        <v-icon start>mdi-home</v-icon>
+        Início
+      </v-btn>
+    </div>
   </v-container>
 </template>
